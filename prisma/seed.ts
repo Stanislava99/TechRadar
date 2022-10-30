3bf30c51e510b29f73d7b298c6524773cb91a6f5
Andrej Gjeorgiev <Andrej.Gjeorgiev@netcetera.com>
2022-10-30 13:47:31 +0100

    seed update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,17 +13,6 @@ async function seed() {
 
   const hashedPassword = await bcrypt.hash("123456789", 10);
 
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
-
   const users = [
     {
       id: "1",
@@ -100,6 +89,15 @@ async function seed() {
     {
       id: "9",
       email: "adreas.dejung@netcetera.com",
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+    {
+      id: "10",
+      email: "aleksandar.apostolovski@netcetera.com",
       password: {
         create: {
           hash: hashedPassword,
@@ -204,6 +202,103 @@ async function seed() {
       description:"From a INFOSEC perspective this would hopefully help teams to keep there dependencies up-to-date. From a Project/product and Software engineer perspective this would reduce the amount of time spent on \"trivial\" dependency updates.",
       type: "LANGUAGES",
       currentViabilityLevel: "ADOPT"
+    },
+    {
+      id:"9",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Kotlin",
+      linkToTechnology: "https://kotlinlang.org/",
+      userId: "10",
+      description:"A modern programming language\n" +
+        "that makes developers happier.",
+      type: "LANGUAGES",
+      currentViabilityLevel: "ADOPT"
+    },
+    {
+      id:"10",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Threat Modeling",
+      linkToTechnology: "https://owasp.org/www-community/Threat_Modeling",
+      userId: "10",
+      description:"Threat modeling works to identify, communicate, and understand threats and mitigations within the context of protecting something of value.\n" +
+        "\n" +
+        "A threat model is a structured representation of all the information that affects the security of an application. In essence, it is a view of the application and its environment through the lens of security.\n" +
+        "\n" +
+        "Threat modeling can be applied to a wide range of things, including software, applications, systems, networks, distributed systems, Internet of Things (IoT) devices, and business processes.\n" +
+        "\n" +
+        "A threat model typically includes:\n" +
+        "\n" +
+        "Description of the subject to be modeled\n" +
+        "Assumptions that can be checked or challenged in the future as the threat landscape changes\n" +
+        "Potential threats to the system\n" +
+        "Actions that can be taken to mitigate each threat\n" +
+        "A way of validating the model and threats, and verification of success of actions taken",
+      type: "TECHNIQUES",
+      currentViabilityLevel: "ADOPT"
+    },
+    {
+      id:"11",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Local First Application",
+      linkToTechnology: "https://www.inkandswitch.com/local-first/",
+      userId: "10",
+      description:"In a centralized application, the data on the server is the single source of truth — any modification to the data must go through the server. Local data is subordinate to the server version. This seems like a natural and inevitable choice to enable collaboration among multiple users of the software. Local-first application, or local-first software, is a set of principles that enables both collaboration and local data ownership. It prioritizes the use of local storage and local networks over servers in remote data centers or the cloud. Techniques like conflict-free replicated data types (CRDTs) and peer-to-peer (P2P) networks have the potential to be a foundational technology for realizing local-first software.",
+      type: "TECHNIQUES",
+      currentViabilityLevel: "ASSESS"
+    },
+    {
+      id:"12",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "SPA By Default",
+      linkToTechnology: "https://www.thoughtworks.com/radar/techniques/spa-by-default",
+      userId: "10",
+      description:"The prevalence of teams choosing a single-page application (SPA) when they need a website continues. We remain concerned that people aren't properly recognizing SPAs as an architectural style to begin with; instead they're immediately jumping into framework selection. SPAs incur complexity that simply doesn't exist with traditional server-based websites: issues such as search engine optimization, browser history management, web analytics and first page load time all need to be addressed. Proper analysis and consideration of the trade-offs is required to determine if that complexity is warranted for business or user experience reasons. Too often teams are skipping that trade-off analysis, blindly accepting the complexity of SPAs by default even when business needs don't justify it. We still see some developers who aren't aware of an alternative approach because they've spent their entire career in a framework like React. We believe that many websites will benefit from the simplicity of server-side logic, and we're encouraged by techniques like Hotwire that help close the gap on user experience.",
+      type: "TECHNIQUES",
+      currentViabilityLevel: "HOLD"
+    },
+    {
+      id:"13",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "k6",
+      linkToTechnology: "https://k6.io/",
+      userId: "10",
+      description:"The best developer experience\n" +
+        "for load testing\n" +
+        "Open source load testing tool and SaaS\n" +
+        "for engineering teams",
+      type: "TOOLS",
+      currentViabilityLevel: "ADOPT"
+    },
+    {
+      id:"14",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Apache Superset",
+      linkToTechnology: "https://superset.apache.org/",
+      userId: "10",
+      description:"Superset is fast, lightweight, intuitive, and loaded with options that make it easy for users of all skill sets to explore and visualize their data, from simple line charts to highly detailed geospatial charts.",
+      type: "TOOLS",
+      currentViabilityLevel: "ASSESS"
+    },
+    {
+      id:"15",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Bun",
+      linkToTechnology: "https://bun.sh/",
+      userId: "10",
+      description:"Bun is a fast all-in-one JavaScript runtime\n" +
+        "Bundle, transpile, install and run JavaScript & TypeScript projects — all in Bun. Bun is a new JavaScript runtime with a native bundler, transpiler, task runner and npm client built-in.",
+      type: "PLATFORMS",
+      currentViabilityLevel: "ASSESS"
+    },
+    {
+      id:"16",
+      entryDate: "2022-07-13T19:20:30.451Z",
+      name: "Colima",
+      linkToTechnology: "https://github.com/abiosoft/colima",
+      userId: "10",
+      description:"Container runtimes on macOS (and Linux) with minimal setup.",
+      type: "PLATFORMS",
+      currentViabilityLevel: "TRIAL"
     },
   ];
 
